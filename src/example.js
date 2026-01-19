32edf49babd0aa97fd654ca5a7811175d5284687
edgfb-183 <rqnhkf0f@047756.xyz>
2026-01-19 17:28:50 +0800

    Add example file with intentional linting errors

---
+++ src/example.js
@@ -0,0 +1,11 @@
+// This file has intentional linting errors for demonstration
+const unusedVariable = "This variable is never used";
+
+function greet(name) {
+  console.log("Hello, " + name);
+  return "Greeting sent"
+}
+
+const message = "Welcome to our application"
+
+export default greet
